797a19d3219904da3b49e80a32355298ae05c9f7
snowdreams1006 <snowdreams1006@163.com>
2019-07-29 16:14:29 +0800

    add reverse_test

---
+++ strings/reverse_test.go
@@ -0,0 +1,19 @@
+package strings
+
+import "testing"
+
+func TestReverse(t *testing.T) {
+	cases := []struct {
+		input,expect string
+	}{
+		{"Hello, world", "dlrow ,olleH"},
+		{"Hello, 世界", "界世 ,olleH"},
+		{"", ""},
+	}
+	for _, c := range cases {
+		got := Reverse(c.input)
+		if got != c.expect {
+			t.Errorf("Reverse(%q) == %q, want %q", c.input, got, c.expect)
+		}
+	}
+}
