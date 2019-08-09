9f607490af344633f06da9f3f1cbd34898f47e0b
snowdreams1006 <snowdreams1006@163.com>
2019-08-09 13:37:40 +0800

    TestExchange

--- main/hello_world_test.go
+++ main/hello_world_test.go
@@ -10,8 +10,20 @@ func TestHelloWorld(t *testing.T) {
 }
 
 func TestFib(t *testing.T) {
-	var a = 1
-	var b = 1
+	//var a int = 1
+	//var b int = 1
+
+	//var a = 1
+	//var b = 1
+
+	//var (
+	//	a = 1
+	//	b = 1
+	//)
+
+	//var a, b = 1, 1
+
+	a, b := 1, 1
 
 	fmt.Print(a)
 	for i := 0; i < 6; i++ {
@@ -22,4 +34,17 @@ func TestFib(t *testing.T) {
 		b = temp + b
 	}
 	fmt.Println()
+}
+
+func TestExchange(t *testing.T) {
+	a, b := 1, 2
+	t.Log(a, b)
+
+	a, b = b, a
+	t.Log(a, b)
+
+	temp := a
+	a = b
+	b = temp
+	t.Log(a, b)
 }
