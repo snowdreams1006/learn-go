556211c0a8512cdedd08c7e806075fe61524cf9b
snowdreams1006 <snowdreams1006@163.com>
2019-08-12 12:28:56 +0800

    var days [7]int 	for i := 0; i < 7; i++ { 		days[i] = 1 + i 	} 	// [1 2 3 4 5 6 7] 	t.Log(days)

--- main/hello_world_test.go
+++ main/hello_world_test.go
@@ -53,6 +53,64 @@ func TestConst(t *testing.T) {
 	const pi = 3.14
 	t.Log(pi)
 
+	// cannot assign to pi
 	//pi = 2.828
 	//t.Log(pi)
+}
+
+func TestConstForIota(t *testing.T) {
+	const (
+		Mon = 1 + iota
+		Tue
+		Wed
+		Thu
+		Fri
+		Sat
+		Sun
+	)
+	// 1 2 3 4 5 6 7
+	t.Log(Mon, Tue, Wed, Thu, Fri, Sat, Sun)
+
+	var days [7]int
+	for i := 0; i < 7; i++ {
+		days[i] = 1 + i
+	}
+	// [1 2 3 4 5 6 7]
+	t.Log(days)
+
+	const (
+		Readable = 1 << iota
+		Writing
+		Executable
+	)
+	// 0001 0010 0100 即 1 2 4
+	t.Log(Readable, Writing, Executable)
+
+	// 0111 即 7,表示可读,可写,可执行
+	accessCode := 7
+	t.Log(accessCode&Readable == Readable, accessCode&Writing == Writing, accessCode&Executable == Executable)
+	// 0110 即 6,表示不可读,可写,可执行
+	accessCode = 6
+	t.Log(accessCode&Readable == Readable, accessCode&Writing == Writing, accessCode&Executable == Executable)
+	// 0100 即 4,表示不可读,不可写,可执行
+	accessCode = 4
+	t.Log(accessCode&Readable == Readable, accessCode&Writing == Writing, accessCode&Executable == Executable)
+	// 0000 即 0,表示不可读,不可写,不可执行
+	accessCode = 0
+	t.Log(accessCode&Readable == Readable, accessCode&Writing == Writing, accessCode&Executable == Executable)
+
+	const (
+		B = 1 << (10 * iota)
+		Kb
+		Mb
+		Gb
+		Tb
+		Pb
+	)
+	// 1 1024 1048576 1073741824 1099511627776 1125899906842624
+	t.Log(B, Kb, Mb, Gb, Tb, Pb)
+
+	// 62.9 KB (64,411 字节)
+	size := 64411.0
+	t.Log(size, size/Kb)
 }
