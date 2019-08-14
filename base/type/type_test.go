09bdb1a39dd211c34a1be795b97e51fb8aee3f28
snowdreams1006 <snowdreams1006@163.com>
2019-08-14 15:09:09 +0800

    TestLogicalOperator

--- base/type/type_test.go
+++ base/type/type_test.go
@@ -27,13 +27,63 @@ func TestPointer(t *testing.T) {
 	t.Logf("%T %T", a, aPoi)
 }
 
-func TestString(t *testing.T){
+func TestString(t *testing.T) {
 	var s string
 	t.Log(len(s))
 
 	if s == "" {
-		t.Log("空字符串",s)
-	}else{
-		t.Log("非空字符串",s)
+		t.Log("空字符串", s)
+	} else {
+		t.Log("非空字符串", s)
 	}
+}
+
+func TestArithmeticOperator(t *testing.T) {
+	a := 0
+	// 0
+	t.Log(a)
+
+	a = a + 1
+	// 1
+	t.Log(a)
+
+	a = a * 2
+	// 2
+	t.Log(a)
+
+	a = a % 2
+	// 0
+	t.Log(a)
+
+	a++
+	// 1
+	t.Log(a)
+}
+
+func TestComparisonOperator(t *testing.T) {
+	a, b := 0, 1
+	t.Log(a, b)
+
+	t.Log(a > b)
+	t.Log(a < b)
+	t.Log(a != b)
+}
+
+func TestCompareArray(t *testing.T) {
+	a := [...]int{1, 2, 3}
+	//b := [...]int{2, 4}
+	c := [...]int{1, 3, 3}
+	d := [...]int{1, 2, 4}
+
+	// a == b --> invalid operation: a == b (mismatched types [3]int and [2]int)
+	//t.Log(a == b)
+	t.Log(a == c)
+	t.Log(a == d)
+}
+
+func TestLogicalOperator(t *testing.T) {
+	a, b := true, false
+	t.Log(a, b)
+
+	t.Log(a&&b,a||b,!a,!b)
 }
