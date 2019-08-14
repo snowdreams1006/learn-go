ff4db213da60e1c7b971476c037a9b9437c33cb8
snowdreams1006 <snowdreams1006@163.com>
2019-08-14 15:43:58 +0800

    operator

--- base/type/type_test.go
+++ base/type/type_test.go
@@ -64,9 +64,8 @@ func TestComparisonOperator(t *testing.T) {
 	a, b := 0, 1
 	t.Log(a, b)
 
-	t.Log(a > b)
-	t.Log(a < b)
-	t.Log(a != b)
+	// false true true
+	t.Log(a > b, a < b, a != b)
 }
 
 func TestCompareArray(t *testing.T) {
@@ -85,5 +84,42 @@ func TestLogicalOperator(t *testing.T) {
 	a, b := true, false
 	t.Log(a, b)
 
-	t.Log(a&&b,a||b,!a,!b)
+	// false true false true
+	t.Log(a && b, a || b, !a, !b)
+}
+
+func TestBitOperator(t *testing.T) {
+	a, b := 1, 2
+	t.Log(a, b)
+
+	// 0 3 3
+	t.Log(a&b, a|b, a^b)
+
+	// 2 1
+	t.Log(a<<1, b>>1)
+}
+
+func TestClearZeroOperator(t *testing.T) {
+	// 0 0 1 0
+	t.Log(1&^1, 0&^1, 1&^0, 0&^1)
+
+	const (
+		Readable = 1 << iota
+		Writing
+		Executable
+	)
+	// 0001 0010 0100 即 1 2 4
+	t.Log(Readable, Writing, Executable)
+
+	// 0111 即 7,表示可读,可写,可执行
+	accessCode := 7
+	t.Log(accessCode&Readable == Readable, accessCode&Writing == Writing, accessCode&Executable == Executable)
+
+	// 0111 &^ 0001 = 0110 即清除可读权限
+	accessCode = accessCode &^ Readable
+	t.Log(accessCode&Readable == Readable, accessCode&Writing == Writing, accessCode&Executable == Executable)
+
+	// 0110 &^ 0010 = 0100
+	accessCode = accessCode &^ Writing
+	t.Log(accessCode&Readable == Readable, accessCode&Writing == Writing, accessCode&Executable == Executable)
 }
