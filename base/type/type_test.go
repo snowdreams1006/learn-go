f8e728f471185873f2adcc6dead4fde417235ea3
snowdreams1006 <snowdreams1006@163.com>
2019-08-16 10:28:12 +0800

    varTest

--- base/type/type_test.go
+++ base/type/type_test.go
@@ -2,7 +2,7 @@ package _type
 
 import "testing"
 
-func TestVarZeroValue(t *testing.T) {
+func TestVariableZeroValue(t *testing.T) {
 	var a int
 	var s string
 
@@ -12,11 +12,56 @@ func TestVarZeroValue(t *testing.T) {
 	t.Logf("%d %q", a, s)
 }
 
-func TestVarInitialValue(t *testing.T){
-	var a,b int = 1,2
-	var s = "hello Go"
+func TestVariableInitialValue(t *testing.T) {
+	var a, b int = 1, 2
+	var s string = "hello Go"
 
-	t.Log(a,b,s)
+	// 1 2 hello Go
+	t.Log(a, b, s)
+}
+
+func TestVariableShorter(t *testing.T) {
+	var (
+		a int    = 1
+		b int    = 2
+		s string = "hello go"
+	)
+
+	// 1 2 hello Go
+	t.Log(a, b, s)
+}
+
+func TestVariableTypeDeduction(t *testing.T) {
+	var a, b, s = 1, 2, "hello Go"
+
+	// 1 2 hello Go
+	t.Log(a, b, s)
+}
+
+func TestVariableTypeDeductionShorter(t *testing.T) {
+	a, b, s := 1, 2, "hello Go"
+
+	// 1 2 hello Go
+	t.Log(a, b, s)
+
+	s = "hello golang"
+
+	// 1 2 hello golang
+	t.Log(a, b, s)
+}
+
+var globalTestId = 2
+// globalTestName := "type_test" is not supported
+var globalTestName = "type_test"
+
+func TestVariableScope(t *testing.T) {
+	// 2 type_test
+	t.Log(globalTestId, globalTestName)
+
+	globalTestName = "TestVariableScope"
+
+	// 2 TestVariableScope
+	t.Log(globalTestId, globalTestName)
 }
 
 func TestImplicitTypeConvert(t *testing.T) {
