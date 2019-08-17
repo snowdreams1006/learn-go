dd3d57963f1a77c41ac4b9ab236ed9f989567ea2
snowdreams1006 <snowdreams1006@163.com>
2019-08-17 15:20:40 +0800

    use pointer to modify array

--- base/container/container_test.go
+++ base/container/container_test.go
@@ -1,6 +1,9 @@
 package container
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestArray(t *testing.T) {
 	var arr1 [3]int
@@ -47,4 +50,68 @@ func TestFindMaxInArray(t *testing.T) {
 	}
 
 	t.Log(maxVal, maxIdx)
+}
+
+func TestSumForArray(t *testing.T) {
+	arr := [...]int{2, 4, 6, 8, 10}
+
+	sum := 0
+	for _, v := range arr {
+		sum += v
+	}
+
+	t.Log(sum)
+}
+
+func printArray(arr [5]int) {
+	arr[0] = 666
+	for i, v := range arr {
+		fmt.Println(i, v)
+	}
+}
+
+func TestPrintArray(t *testing.T){
+	var arr1 [3]int
+	arr2 := [5]int{1, 2, 3, 4, 5}
+	arr3 := [...]int{2, 4, 6, 8, 10}
+
+	// [0 0 0] [1 2 3 4 5] [2 4 6 8 10]
+	t.Log(arr1, arr2, arr3)
+
+	// cannot use arr1 (type [3]int) as type [5]int in argument to printArray
+	//printArray(arr1)
+
+	fmt.Println("printArray(arr2)")
+	printArray(arr2)
+
+	fmt.Println("printArray(arr3)")
+	printArray(arr3)
+
+	// [1 2 3 4 5] [2 4 6 8 10]
+	t.Log(arr2,arr3)
+}
+
+func printArrayByPointer(arr *[5]int) {
+	arr[0] = 666
+	for i, v := range arr {
+		fmt.Println(i, v)
+	}
+}
+
+func TestPrintArrayByPointer(t *testing.T){
+	var arr1 [3]int
+	arr2 := [5]int{1, 2, 3, 4, 5}
+	arr3 := [...]int{2, 4, 6, 8, 10}
+
+	// [0 0 0] [1 2 3 4 5] [2 4 6 8 10]
+	t.Log(arr1, arr2, arr3)
+
+	fmt.Println("printArrayByPointer(arr2)")
+	printArrayByPointer(&arr2)
+
+	fmt.Println("printArrayByPointer(arr3)")
+	printArrayByPointer(&arr3)
+
+	// [666 2 3 4 5] [666 4 6 8 10]
+	t.Log(arr2,arr3)
 }
