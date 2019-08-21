0e7cb8eaaaae2e0b4178ac4f5ab59aa9128243c1
snowdreams1006 <snowdreams1006@163.com>
2019-08-21 17:37:32 +0800

    isempty

--- oop/struct/stuct_test.go
+++ oop/struct/stuct_test.go
@@ -3,6 +3,7 @@ package _struct
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"testing"
 	"unsafe"
 )
@@ -83,10 +84,114 @@ var dir []point = []point{
 	{0, 1},
 }
 
-func TestPoint(t *testing.T){
-	if file,err := os.Open("container/maza/maze.in");err != nil{
+func TestPoint(t *testing.T) {
+	if file, err := os.Open("container/maza/maze.in"); err != nil {
 		panic(err)
-	}else{
+	} else {
 		t.Log(file)
 	}
+}
+
+type MyDynamicArray struct {
+	ptr *[10]int
+	len int
+	cap int
+}
+
+func NewMyDynamicArray() *MyDynamicArray {
+	var myDynamicArray MyDynamicArray
+
+	myDynamicArray.len = 0
+	myDynamicArray.cap = 10
+	var arr [10]int
+	myDynamicArray.ptr = &arr
+
+	return &myDynamicArray
+}
+
+func (myArr *MyDynamicArray) AddLast(value int) {
+	myArr.Add(myArr.len, value)
+}
+
+func (myArr *MyDynamicArray) AddFirst(value int) {
+	myArr.Add(0, value)
+}
+
+func (myArr *MyDynamicArray) Add(index, value int) {
+	if myArr.len == myArr.cap {
+		return
+	}
+
+	if index < 0 || index > myArr.len {
+		return
+	}
+
+	for i := myArr.len - 1; i >= index; i-- {
+		(*myArr.ptr)[i+1] = (*myArr.ptr)[i]
+	}
+
+	(*myArr.ptr)[index] = value
+	myArr.len++
+}
+
+func (myArr *MyDynamicArray) Set(index, value int) {
+	if index < 0 || index >= myArr.len {
+		return
+	}
+
+	(*myArr.ptr)[index] = value
+}
+
+func (myArr *MyDynamicArray) Get(index int) int {
+	if index < 0 || index >= myArr.len {
+		return -1
+	}
+
+	return (*myArr.ptr)[index]
+}
+
+func (myArr *MyDynamicArray) GetLen() int {
+	return myArr.len
+}
+
+func (myArr *MyDynamicArray) GetCap() int {
+	return myArr.cap
+}
+
+func (myArr *MyDynamicArray) IsEmpty() bool {
+	return myArr.len == 0
+}
+
+func (myArr *MyDynamicArray) Print() string {
+	res := fmt.Sprintf("Array: len = %d , cap = %d\n", myArr.len, myArr.cap)
+	res = res + "["
+
+	for i := 0; i < myArr.len; i++ {
+		res = res + strconv.Itoa((*myArr.ptr)[i])
+		if i != myArr.len-1 {
+			res = res + ", "
+		}
+	}
+
+	res = res + "]"
+	return res
+}
+
+func TestMyDynamicArray(t *testing.T) {
+	myDynamicArray := NewMyDynamicArray()
+
+	for i := 0; i < myDynamicArray.cap; i++ {
+		myDynamicArray.AddLast(i)
+	}
+
+	t.Logf("array = %v,len = %d,cap = %d", *myDynamicArray.ptr, myDynamicArray.len, myDynamicArray.cap)
+
+	fmt.Println(myDynamicArray.Print())
+
+	myDynamicArray.Set(6,666)
+
+	fmt.Println(myDynamicArray.Print())
+
+	fmt.Println(myDynamicArray.Get(6))
+	fmt.Println(myDynamicArray.IsEmpty())
 }
