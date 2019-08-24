6996fd5c41bd265814121f66d544ec56d8be6f6c
snowdreams1006 <snowdreams1006@163.com>
2019-08-24 22:41:10 +0800

    cat and dog

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # learn-go
 
+
+[https://snowdreams1006.github.io/go/](https://snowdreams1006.github.io/go/)
+
 - `go get github.com/snowdreams1006/learn-go/hello`
 - `go test github.com/snowdreams1006/learn-go/strings`
 - `go install github.com/snowdreams1006/learn-go/hello`
--- oop/extend/extend_test.go
+++ oop/extend/extend_test.go
@@ -8,6 +8,10 @@ import (
 type Pet struct {
 }
 
+func (p *Pet) Skill() {
+	fmt.Println("能文能武的宠物")
+}
+
 func (p *Pet) Speak() {
 	fmt.Print("...")
 }
@@ -21,6 +25,10 @@ type Dog struct {
 	p *Pet
 }
 
+func (d *Dog) Navigate() {
+	fmt.Println("自带导航汪汪汪")
+}
+
 func (d *Dog) Speak() {
 	fmt.Println("Wang")
 }
@@ -29,7 +37,37 @@ func (d *Dog) SpeakTo(host string) {
 	d.p.SpeakTo(host)
 }
 
-func TestDog(t *testing.T){
+type Cat struct {
+	p *Pet
+}
+
+func (c *Cat) Catch() {
+	fmt.Println("老鼠天敌喵喵喵")
+}
+
+func TestDog(t *testing.T) {
 	d := new(Dog)
 	d.SpeakTo("Chao")
+}
+
+func TestExtendInstance(t *testing.T) {
+	p := new(Pet)
+
+	d := new(Dog)
+	d.p = p
+
+	// 自带导航汪汪汪
+	d.Navigate()
+	// 能文能武的宠物
+	d.p.Skill()
+
+	fmt.Println()
+
+	c := new(Cat)
+	c.p = p
+
+	// 老鼠天敌喵喵喵
+	c.Catch()
+	// 能文能武的宠物
+	c.p.Skill()
 }
