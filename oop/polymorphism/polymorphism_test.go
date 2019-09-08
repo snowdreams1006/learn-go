51f8553a026c4856f3862376e69c4df6e359f154
snowdreams1006 <snowdreams1006@163.com>
2019-09-08 11:01:44 +0800

    add poly test

--- oop/polymorphism/polymorphism_test.go
+++ oop/polymorphism/polymorphism_test.go
@@ -12,7 +12,6 @@ type Programmer interface {
 }
 
 type GoProgrammer struct {
-
 }
 
 func (g *GoProgrammer) WriteHelloWord() Code {
@@ -45,8 +44,47 @@ type MyProgrammer interface {
 }
 
 func TestInterfaceType(t *testing.T) {
-	var p Programmer = new(GoProgrammer)
+	var _ Programmer = new(GoProgrammer)
 	//var _ MyProgrammer = new(JavaProgrammer)
 }
 
+type Equaler interface {
+	Equal(Equaler) bool
+}
 
+type T int
+
+func (t T) Equal(u T) bool { return t == u }
+
+func TestEqualFail(t *testing.T) {
+	//var _ Equaler = T{}
+}
+
+type T2 int
+
+func (t T2) Equal(u Equaler) bool { return t == u.(T2) }
+
+func TestEqualPass(t *testing.T) {
+	var _ Equaler = (*T2)(nil)
+}
+
+type T3 int
+
+func (t T3) Equal(u Equaler) bool { return t == u.(T3) }
+
+func TestEqualType(t *testing.T) {
+	var t2 Equaler = new(T2)
+	var t3 Equaler = new(T3)
+
+	t.Logf("%[1]T %[1]v\n",t2)
+	t.Logf("%[1]T %[1]v\n",t3)
+	t.Logf("%[1]T %[1]v %v\n",t2,t2.Equal(t3))
+}
+
+func TestInterfaceTypeDeduce(t *testing.T) {
+	var t2 Equaler = new(T2)
+	var t3 Equaler = new(T3)
+
+	t.Logf("%[1]T %[1]v %[2]T %[2]v\n",t2,t2.(*T2))
+	t.Logf("%[1]T %[1]v %[2]T %[2]v\n",t3,t3.(*T3))
+}
