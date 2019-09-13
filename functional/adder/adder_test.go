3bf8fea5bdac4ab1370c94bfe05d007ba31a9f43
snowdreams1006 <snowdreams1006@163.com>
2019-09-13 14:15:52 +0800

    TestMAdder

--- functional/adder/adder_test.go
+++ functional/adder/adder_test.go
@@ -16,3 +16,22 @@ func TestAdder(t *testing.T) {
 		t.Logf("0+...+%d=%d", i, a(i))
 	}
 }
+
+type iAdder func(int) (int, iAdder)
+
+func mAdder(base int) iAdder {
+	return func(v int) (int, iAdder) {
+		return base + v, mAdder(base + v)
+	}
+}
+
+func TestMAdder(t *testing.T){
+	m := mAdder(0)
+
+	for i := 0; i < 10; i++ {
+		var s int
+		s,m = m(i)
+
+		t.Logf("0+...+%d=%d", i, s)
+	}
+}
