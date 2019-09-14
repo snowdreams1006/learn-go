c2af1d91e88f519251bfb4b90b709ed86ee0682d
snowdreams1006 <snowdreams1006@163.com>
2019-09-14 10:03:41 +0800

    add fib

--- functional/fib/fib_test.go
+++ functional/fib/fib_test.go
@@ -1,10 +1,16 @@
 package fib
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"strings"
 	"testing"
 )
 
+// 1 1 2 3 5 8 13 21 34 55
+//     a b
+//       a b
 func fibonacci() func() int {
 	a, b := 0, 1
 	return func() int {
@@ -16,7 +22,39 @@ func fibonacci() func() int {
 func TestFibonacci(t *testing.T) {
 	f := fibonacci()
 	for i := 0; i < 10; i++ {
-		fmt.Print(f()," ")
+		fmt.Print(f(), " ")
 	}
 	fmt.Println()
+}
+
+func printFileContents(reader io.Reader) {
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func fib() intGen {
+	a, b := 0, 1
+	return func() int {
+		a, b = b, a+b
+		return a
+	}
+}
+
+type intGen func() int
+
+func (g intGen) Read(p []byte) (n int, err error) {
+	next := g()
+	if next > 10000 {
+		return 0, io.EOF
+	}
+	s := fmt.Sprintf("%d\n", next)
+	// TODO incorrect if p is too small
+	return strings.NewReader(s).Read(p)
+}
+
+func TestIntGenRead(t *testing.T) {
+	f := fib()
+	printFileContents(f)
 }
