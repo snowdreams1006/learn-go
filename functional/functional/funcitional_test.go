b5fa75bce704fc3e3f3f4465c8fe08faedb5ee6e
snowdreams1006 <snowdreams1006@163.com>
2019-09-15 16:50:03 +0800

    TestEvalWithApplyStyle

--- functional/functional/funcitional_test.go
+++ functional/functional/funcitional_test.go
@@ -2,6 +2,9 @@ package functional
 
 import (
 	"fmt"
+	"math"
+	"reflect"
+	"runtime"
 	"testing"
 )
 
@@ -62,4 +65,67 @@ func TestEvalWithStandardStyle(t *testing.T) {
 	} else {
 		t.Log("Success:", result)
 	}
+}
+
+func evalWithApplyStyle(a, b int, op func(int, int) (int, error)) (int, error) {
+	return op(a, b)
+}
+
+func divide(a, b int) (int, error) {
+	return a / b, nil
+}
+
+func mod(a, b int) (int, error) {
+	return a % b, nil
+}
+
+func TestEvalWithApplyStyle(t *testing.T) {
+	// Success: 2
+	if result, err := evalWithApplyStyle(5, 2, divide); err != nil {
+		t.Log("Error:", err)
+	} else {
+		t.Log("Success:", result)
+	}
+
+	// Success: 1
+	if result, err := evalWithApplyStyle(5, 2, mod); err != nil {
+		t.Log("Error:", err)
+	} else {
+		t.Log("Success:", result)
+	}
+}
+
+func apply(op func(int, int) int, a, b int) int {
+	p := reflect.ValueOf(op).Pointer()
+	opName := runtime.FuncForPC(p).Name()
+
+	fmt.Printf("Calling function %s with args (%d,%d)\n", opName, a, b)
+	return op(a, b)
+}
+
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+func TestApply(t *testing.T) {
+	// 1
+	t.Log(apply(func(a int, b int) int {
+		return a % b
+	}, 5, 2))
+
+	// 25
+	t.Log(apply(pow, 5, 2))
+}
+
+func sum(numbers ...int) int {
+	result := 0
+	for i := range numbers {
+		result += numbers[i]
+	}
+	return result
+}
+
+func TestSum(t *testing.T) {
+	// 15
+	t.Log(sum(1, 2, 3, 4, 5))
 }
