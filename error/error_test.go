f2b4410396800cbaf08fe169007162fd30a48751
snowdreams1006 <snowdreams1006@163.com>
2019-10-14 00:00:18 +0800

    add TestWriteFileWithDefer

--- error/error_test.go
+++ error/error_test.go
@@ -1,6 +1,9 @@
 package error
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"testing"
 )
 
@@ -39,4 +42,30 @@ func TestFuncWithMultipleDeferAndPanic(t *testing.T) {
 	t.Log(3)
 	panic("「雪之梦技术驿站」: defer 在于保证无论正常代码如何执行,该逻辑代码也一定会执行!")
 	t.Log(4)
+}
+
+func fibonacciWithClosure() func() int {
+	a, b := 0, 1
+	return func() int {
+		a, b = b, a+b
+		return a
+	}
+}
+
+func TestWriteFileWithDefer(t *testing.T) {
+	//「雪之梦技术驿站」: 有始有终,打开过文件要及时关闭,defer 让我们操作变得有始有终!
+	if file, err := os.Create("fib.txt"); err != nil {
+		panic(err)
+	} else {
+		defer file.Close()
+
+		//「雪之梦技术驿站」: bufio 暂存内存要刷新到文件,defer 帮助我们及时进行资源管理,出入成双!
+		writer := bufio.NewWriter(file)
+		defer writer.Flush()
+
+		f := fibonacciWithClosure()
+		for i := 0; i < 10; i++ {
+			fmt.Fprintln(writer, f())
+		}
+	}
 }
