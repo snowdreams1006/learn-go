6af64ab2d1935cdf0a0c68c53c30f5511fdbdf34
snowdreams1006 <snowdreams1006@163.com>
2019-10-14 21:28:01 +0800

    TestPanicWithPanic

--- error/recover_test.go
+++ error/recover_test.go
@@ -5,7 +5,30 @@ import (
 	"testing"
 )
 
-func TestPanic(t *testing.T) {
+func TestPanicWithRecover(t *testing.T) {
+	defer func() {
+		r := recover()
+		if err, ok := r.(error); ok {
+			t.Logf("Error occurred : %s", err.Error())
+		}else{
+			panic(r)
+		}
+	}()
+
 	// 「雪之梦技术驿站」: This is an error
 	panic(errors.New("「雪之梦技术驿站」: This is an error"))
+}
+
+func TestPanicWithPanic(t *testing.T) {
+	defer func() {
+		r := recover()
+		if err, ok := r.(error); ok {
+			t.Logf("Error occurred : %s", err.Error())
+		}else{
+			panic(r)
+		}
+	}()
+
+	// 「雪之梦技术驿站」: This is a custom statement
+	panic("「雪之梦技术驿站」: This is a custom statement")
 }
