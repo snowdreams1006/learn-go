6fe395d72c425fa2676b3b6389047a9f17dc1936
snowdreams1006 <snowdreams1006@163.com>
2019-10-15 11:23:40 +0800

    add f33

--- error/error_test.go
+++ error/error_test.go
@@ -226,3 +226,80 @@ func TestPanicVxExit3(t *testing.T) {
 	t.Log("start")
 	panic(errors.New("something wrong"))
 }
+
+func f1() (result int) {
+	defer func() {
+		result++
+	}()
+	return 0
+}
+
+func TestF1(t *testing.T) {
+	// 1
+	t.Log(f1())
+}
+
+func f11() (result int) {
+	result = 0  //return语句不是一条原子调用，return xxx其实是赋值＋ret指令
+	func() { //defer被插入到return之前执行，也就是赋返回值和ret指令之间
+		result++
+	}()
+	return
+}
+
+func TestF11(t *testing.T) {
+	// 1
+	t.Log(f11())
+}
+
+func f2() (r int) {
+	t := 5
+	defer func() {
+		t = t + 5
+	}()
+	return t
+}
+
+func TestF2(t *testing.T) {
+	// 5
+	t.Log(f2())
+}
+
+func f22() (r int) {
+	t := 5
+	r = t //赋值指令
+	func() {        //defer被插入到赋值与返回之间执行，这个例子中返回值r没被修改过
+		t = t + 5
+	}()
+	return        //空的return指令
+}
+
+func TestF22(t *testing.T) {
+	// 5
+	t.Log(f22())
+}
+
+func f3() (r int) {
+	defer func(r int) {
+		r = r + 5
+	}(r)
+	return 1
+}
+
+func TestF3(t *testing.T) {
+	// 1
+	t.Log(f3())
+}
+
+func f33() (r int) {
+	r = 1  //给返回值赋值
+	func(r int) {        //这里改的r是传值传进去的r，不会改变要返回的那个r值
+		r = r + 5
+	}(r)
+	return        //空的return
+}
+
+func TestF33(t *testing.T) {
+	// 1
+	t.Log(f33())
+}
